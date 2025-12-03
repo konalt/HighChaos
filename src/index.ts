fa102d7f2013a8bd18729859e7cc49d6e023d6d2
konalt <danannlawless@gmail.com>
2025-12-03 23:07:17 +0000

    A9ESG9HA9T LOOK AT ME I CAN TYPE

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import * as cutscene_intro from "./cutscenes/cutscene_intro";
+import * as scene_menu from "./scenes/menu";
 import { w, h } from "./engine";
 import * as c from "./engine";
-import menu from "./menu";
+import { Scene } from "./utils";
 
-cutscene_intro.init();
-c.setDrawFunction(cutscene_intro.draw);
+function setScene(scene: Scene) {
+    scene.init();
+    c.setDrawFunction(scene.draw);
+}
+
+setScene(scene_menu);
 
 c.init();
