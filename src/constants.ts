24f88ffcb38ef62428bc944f10aacb847224050f
konalt <danannlawless@gmail.com>
2025-12-07 23:31:48 +0000

    change to woff2, add oxanium

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const MenuFont = "'Futuristic Armour', sans-serif";
+export const MenuFontTitle = "'Futuristic Armour', sans-serif";
+export const MenuFont = "'Oxanium Regular', sans-serif";
 
 export const FadeDuration = 2000;
--- src/fonts.ts
+++ src/fonts.ts
@@ -1,8 +1,17 @@
-const fontNames = ["Futuristic-Armour"];
+const FontWeights = ["ExtraLight", "Light", "Regular", "Medium", "SemiBold", "Bold", "ExtraBold"];
+const FontsWithWeights = ["Oxanium"];
+
+let fontNames = ["Futuristic-Armour"];
+
+for (const wt of FontWeights) {
+    for (const f of FontsWithWeights) {
+        fontNames.push(`${f}/${f}-${wt}`);
+    }
+}
 
 export async function loadFonts() {
     for (const fn of fontNames) {
-        const f = new FontFace(fn.replace(/-/g, " "), `url("fonts/${fn}.woff")`);
+        const f = new FontFace(fn.split("/").at(-1).replace(/-/g, " "), `url("fonts/${fn}.woff2")`);
         document.fonts.add(f);
         f.load();
     }
