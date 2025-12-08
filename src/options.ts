b288a2f2d563c03ea2f0687175c3a706eff15b5e
konalt <danannlawless@gmail.com>
2025-12-08 20:33:23 +0000

    details!

--- src/options.ts
+++ src/options.ts
@@ -13,7 +13,7 @@ export const ResolutionOptions: [number, string][] = [
 export type DetailLevel = 0 | 1 | 2 | 3;
 export const DetailLevelNames = ["Minimal", "Low", "Medium", "High"];
 export function detail(required = 3) {
-    return required >= settings.detailLevel;
+    return required <= settings.detailLevel;
 }
 
 export interface Settings {
@@ -54,4 +54,14 @@ export function prevResolution() {
     settings.resolution--;
     if (settings.resolution < 0) settings.resolution = ResolutionOptions.length - 1;
     setResolution(ResolutionOptions[settings.resolution][0]);
+}
+
+export function nextDetail() {
+    settings.detailLevel++;
+    if (settings.detailLevel >= 4) settings.detailLevel = 0;
+}
+
+export function prevDetail() {
+    settings.detailLevel--;
+    if (settings.detailLevel < 0) settings.detailLevel = 3;
 }
