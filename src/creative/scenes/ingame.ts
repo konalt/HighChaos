1701f3a33eb1e041d63e50b47da6110def01425b
konalt <danannlawless@gmail.com>
2026-03-15 17:02:11 +0000

    add nice background

--- src/creative/scenes/ingame.ts
+++ src/creative/scenes/ingame.ts
@@ -1,12 +1,23 @@
+import { h } from "../../lib/engine/engine";
 import { Scene } from "../../lib/engine/scene";
-import { Background } from "../../lib/ui/background";
-import { Sky } from "../objects/sky";
+import { GradientType } from "../../lib/engine/utils";
+import { GradientBackground } from "../../lib/ui/background/gradientbackground";
 
 export class InGameScene extends Scene {
+    background: GradientBackground;
+
     constructor() {
         super();
 
-        let background = new Background();
-        background.color = "red";
+        this.background = new GradientBackground();
+        this.background.type = GradientType.Radial;
+        this.background.colors = ["#3098fa", "#184fe6"];
+        this.background.scale = 1.5;
+        this.background.y = h / 2;
+        this.add(this.background);
+    }
+
+    update(): void {
+        super.update();
     }
 }
