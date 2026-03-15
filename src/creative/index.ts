a55a70e0621350db55f4d70a7adb58bf48655d20
konalt <danannlawless@gmail.com>
2026-03-15 20:04:36 +0000

    chang efont and remove useless

--- src/creative/index.ts
+++ src/creative/index.ts
@@ -6,13 +6,13 @@ import init from "../wasm_gzip/wasm_gzip.js";
 import { ConnectScene } from "./scenes/connect";
 
 Promise.all([init(), loadFonts()]).then(() => {
-    c.setFont("'Futuristic Armour', sans-serif");
+    c.setFont("'Le Cristal de Lune', cursive");
 
     loadSettings();
 
     c.setResolution(1);
 
-    c.setScene(new ConnectScene(), false).then(() => {
+    c.setScene(new ConnectScene()).then(() => {
         c.init("creative");
     });
 });
