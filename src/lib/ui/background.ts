3b7ca51005af623e11fb52282fae9bd62646a4fe
konalt <danannlawless@gmail.com>
2026-03-15 13:40:21 +0000

    background now smaller

--- src/lib/ui/background.ts
+++ src/lib/ui/background.ts
@@ -2,12 +2,8 @@ import { CanvasStyle, d, h, w } from "../engine/engine";
 import { GameObject } from "../engine/object";
 
 export class Background extends GameObject {
-    color: CanvasStyle;
-
     constructor() {
         super();
-
-        this.color = "#000";
     }
 
     draw() {
