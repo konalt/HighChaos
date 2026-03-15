68ef354b1e84d0f78d625ba7af46a94c0daec9f2
konalt <danannlawless@gmail.com>
2026-03-15 13:38:31 +0000

    add color to gameobjects

--- src/lib/engine/object.ts
+++ src/lib/engine/object.ts
@@ -1,13 +1,16 @@
+import { CanvasStyle } from "./engine";
 import { Scene } from "./scene";
 
 export class GameObject {
     x: number;
     y: number;
+    color: CanvasStyle;
     scene: Scene;
 
     constructor() {
         this.x = 0;
         this.y = 0;
+        this.color = "#ffffff";
     }
 
     draw() {
