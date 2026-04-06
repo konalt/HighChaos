d0db11db4f888a2d6cebf5c9c2c98ab72a6fce64
konalt <danannlawless@gmail.com>
2026-04-06 13:23:46 +0100

    add clickable buttons

--- src/creative/objects/ui/hotbar.ts
+++ src/creative/objects/ui/hotbar.ts
@@ -1,6 +1,6 @@
-import { ctx, d, getKeyDown, h, w } from "../../../lib/engine/engine";
+import { ctx, CursorMode, d, getKeyDown, getMouse, h, setCursorMode, w } from "../../../lib/engine/engine";
 import { GameObject } from "../../../lib/engine/object";
-import { anchorToCoords, FourNums } from "../../../lib/engine/utils";
+import { anchorToCoords, basicPointInRect, FourNums } from "../../../lib/engine/utils";
 import { hotbar, hotbarSlot, selectHotbarSlot, UI_COLOR } from "../../game/game";
 import { InGameScene } from "../../scenes/ingame";
 import { drawBlockRaw } from "../world";
@@ -11,12 +11,15 @@ const HOTBAR_PADDING_Y = 15;
 const HOTBAR_GAP = 10;
 const HOTBAR_DESELECTED_SCALE = 0.85;
 const HOTBAR_ROUND = 10;
+const HOTBAR_SELECT_INDICATOR_SIZE = 2;
 
 const HOTBAR_SELECT_KEYS: [number, string][] = new Array(9).fill(0).map((_, i) => [i, `Digit${i + 1}`]);
 
 export class Hotbar extends GameObject {
     private _width = 0;
     private _height = 0;
+
+    private _hoveredIndex = -1;
 
     needsUpdate = true;
 
@@ -40,6 +43,11 @@ export class Hotbar extends GameObject {
                 selectHotbarSlot(ind);
             }
         }
+
+        this._hoveredIndex = -1;
+        for (let i = 0; i < hotbar.length; i++) {
+            this._checkItemClick(i);
+        }
     }
 
     draw() {
@@ -60,33 +68,58 @@ export class Hotbar extends GameObject {
         }
     }
 
-    private _drawItem(index: number) {
-        let type = hotbar[index];
-
-        /* if (type == -1) {
-            // empty slot
-            return;
-        } */
-
+    private _getItemCoords(index: number): FourNums {
         let selected = index == hotbarSlot;
 
         let x = (w - this._width) / 2 + HOTBAR_PADDING_X + (HOTBAR_ITEM_SIZE + HOTBAR_GAP) * index;
         let y = h - this._height + HOTBAR_PADDING_Y;
 
-        if (!selected) {
-            ctx.globalAlpha = 0.5;
+        if (selected) {
+            let s = HOTBAR_ITEM_SIZE;
+            return [x, y, s, s];
+        } else {
+            let s = HOTBAR_ITEM_SIZE * HOTBAR_DESELECTED_SCALE;
+            let o = (HOTBAR_ITEM_SIZE * (1 - HOTBAR_DESELECTED_SCALE)) / 2;
+            return [x + o, y + o, s, s];
+        }
+    }
 
-            drawBlockRaw(
-                x + (HOTBAR_ITEM_SIZE * (1 - HOTBAR_DESELECTED_SCALE)) / 2,
-                y + (HOTBAR_ITEM_SIZE * (1 - HOTBAR_DESELECTED_SCALE)) / 2,
-                HOTBAR_ITEM_SIZE * HOTBAR_DESELECTED_SCALE,
-                HOTBAR_ITEM_SIZE * HOTBAR_DESELECTED_SCALE,
-                type,
-            );
+    private _checkItemClick(index: number) {
+        let rect = this._getItemCoords(index);
+        let mouse = getMouse(true);
+
+        if (basicPointInRect(...mouse, ...rect)) {
+            setCursorMode(CursorMode.Click);
+            this._hoveredIndex = index;
+            if (getKeyDown("mouse1")) {
+                selectHotbarSlot(index);
+            }
+        }
+    }
+
+    private _drawItem(index: number) {
+        let type = hotbar[index];
+
+        let selected = index == hotbarSlot;
+        let hovered = index == this._hoveredIndex;
+
+        let rect = this._getItemCoords(index);
+
+        if (!selected) {
+            ctx.globalAlpha = hovered ? 0.7 : 0.5;
+            drawBlockRaw(...rect, type);
             ctx.globalAlpha = 1;
         } else {
-            drawBlockRaw(x, y, HOTBAR_ITEM_SIZE, HOTBAR_ITEM_SIZE, type);
-            d.rect(x - 2, y - 2, HOTBAR_ITEM_SIZE + 4, HOTBAR_ITEM_SIZE + 4, "transparent", "white", 3);
+            drawBlockRaw(...rect, type);
+            d.rect(
+                rect[0] - HOTBAR_SELECT_INDICATOR_SIZE,
+                rect[1] - HOTBAR_SELECT_INDICATOR_SIZE,
+                rect[2] + HOTBAR_SELECT_INDICATOR_SIZE * 2,
+                rect[3] + HOTBAR_SELECT_INDICATOR_SIZE * 2,
+                "transparent",
+                "white",
+                3,
+            );
         }
     }
 }
