7149f02060621c16c516401c15b25a9053b3e16c
konalt <danannlawless@gmail.com>
2026-06-19 19:47:16 +0100

    rmeove the useless comment

--- src/empty/index.ts
+++ src/empty/index.ts
@@ -1,10 +1,8 @@
 import * as c from "../lib/engine/engine";
 import { loadFonts } from "../lib/engine/fonts";
-import { loadSettings } from "../lib/engine/options";
 import { ExampleScene } from "./scenes/example";
 
 loadFonts().then(() => {
-    //loadSettings();
     c.setResolution(1);
 
     c.setScene(new ExampleScene(), true);
