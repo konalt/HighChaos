6f481279e5ccf8b1269f7c3bf655c16395a45678
konalt <danannlawless@gmail.com>
2026-06-20 21:34:43 +0100

    change main menu colors

--- src/cahv2/color.ts
+++ src/cahv2/color.ts
@@ -1,6 +1,6 @@
 export const COLOR = {
-    backgroundDark: "#055d97",
-    backgroundLight: "#46b7da",
+    backgroundDark: "#0d0911",
+    backgroundLight: "#2b2e61",
     elementStroke: "#8effff",
     elementFill: "rgba(85,131,255,0.4)",
     cardWhite: "#e5e5e5",
