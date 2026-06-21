f2739a355d13644ce412277741965cbae1037339
konalt <danannlawless@gmail.com>
2026-06-22 00:05:38 +0100

    new background effect

--- src/cahv2/scenes/menubase.ts
+++ src/cahv2/scenes/menubase.ts
@@ -3,14 +3,15 @@ import { Scene, UI_LAYER } from "../../lib/engine/scene";
 import { GradientType } from "../../lib/engine/utils";
 import { GradientBackground } from "../../lib/ui/background/gradientbackground";
 import { CAHBackground } from "../objects/ui/background";
+import { CAHBetterBackground } from "../objects/ui/betterbackground";
 
 export class CAHMenuBaseScene extends Scene {
-    background: CAHBackground;
+    background: CAHBetterBackground;
 
     constructor() {
         super();
 
-        this.background = new CAHBackground();
+        this.background = new CAHBetterBackground();
         this.add(this.background, UI_LAYER);
     }
 }
