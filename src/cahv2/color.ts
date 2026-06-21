a594b581b79dde4b916535d45bcf1edefd8728e6
konalt <danannlawless@gmail.com>
2026-06-21 15:14:53 +0100

    change da colorus

--- src/cahv2/color.ts
+++ src/cahv2/color.ts
@@ -1,6 +1,6 @@
 export const COLOR = {
-    backgroundDark: "#0d0911",
-    backgroundLight: "#2b2e61",
+    backgroundDark: "#11021f",
+    backgroundLight: "#640f75",
     elementStroke: "#8effff",
     elementFill: "rgba(85,131,255,0.4)",
     cardWhite: "#e5e5e5",
