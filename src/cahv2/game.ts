1095e2a0f709f138a17e3637f59dc1143f675b64
konalt <danannlawless@gmail.com>
2026-06-30 00:56:00 +0100

    HADAMTADADHUSDGHASD

--- src/cahv2/game.ts
+++ src/cahv2/game.ts
@@ -1,7 +1,13 @@
-import { CAHGame } from "./types";
+import { CAHGame, CAHPlayer } from "./types";
 
 export let currentGame: CAHGame;
 
 export function setGame(game: CAHGame) {
     currentGame = game;
+}
+
+export let currentPlayer: CAHPlayer;
+
+export function setPlayer(ply: CAHPlayer) {
+    currentPlayer = ply;
 }
